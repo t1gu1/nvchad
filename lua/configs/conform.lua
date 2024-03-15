4cd21571323bac1399fcf826b209e63de126bed0
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-03-15 02:49:14 -0400

    Make formatting works for few projects

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,9 +1,17 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    lua = { "stylua" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		typescript = { { "prettierd", "prettier" } },
+		typescriptreact = { { "prettierd", "prettier" } },
+		javascript = { { "prettierd", "prettier" } },
+		javascriptreact = { { "prettierd", "prettier" } },
+		json = { { "prettierd", "prettier" } },
+		html = { { "prettierd", "prettier" } },
+		css = { { "prettierd", "prettier" } },
+		astro = { { "prettierd", "prettier" } },
+	},
 }
 
 require("conform").setup(options)
