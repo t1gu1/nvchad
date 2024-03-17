bef031fb51efca305586c57f5ed9d3d2a1efa053
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-03-17 05:29:34 -0400

    Make the terminal appear bigger

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -10,6 +10,19 @@ M.ui = {
 	tabufline = {
 		order = { "buffers", "tabs", "btns" }, -- Add "treeOffset" if you dont use nvimTreee like a popup
 	},
+
+	term = {
+		-- hl = "Normal:term,WinSeparator:WinSeparator",
+		-- sizes = { sp = 0.3, vsp = 0.2 },
+		float = {
+			relative = "editor",
+			row = 0.1,
+			col = 0.045,
+			width = 0.9,
+			height = 0.8,
+			border = "single",
+		},
+	},
 }
 
 M.plugins = "plugins"
