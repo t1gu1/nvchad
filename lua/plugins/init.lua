aafbbb894c8f65d456c72dae3372a3140ce92747
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-04-07 00:40:02 -0400

    Add undo tree and update some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -246,6 +246,26 @@ return {
 			require("mini.move").setup()
 		end,
 	},
+	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = { -- note how they're inverted to above example
+			{
+				"nvim-telescope/telescope.nvim",
+				dependencies = { "nvim-lua/plenary.nvim" },
+			},
+		},
+		keys = {
+			{ -- lazy style key map
+				"<leader>u",
+				"<cmd>Telescope undo<cr>",
+				desc = "undo history",
+			},
+		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
+			require("telescope").load_extension("undo")
+		end,
+	},
 
 	-- LSP
 	{
