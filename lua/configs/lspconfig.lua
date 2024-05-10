bcdc39ae0737e85c19fa2b31840aa66365b6f364
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-05-10 14:39:01 -0400

    Filter the "index.d.ts" file from the "gd" files possibilities

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -9,9 +9,49 @@ local servers = {
 	gdscript = {},
 }
 
+-- Utils functions to redefine the "gd" function to remove the index.d.ts files from GD
+local function filter(arr, fn)
+	if type(arr) ~= "table" then
+		return arr
+	end
+
+	local filtered = {}
+	for k, v in pairs(arr) do
+		if fn(v, k, arr) then
+			table.insert(filtered, v)
+		end
+	end
+
+	return filtered
+end
+
+local function filterReactDTS(value)
+	return string.match(value.filename, "index.d.ts") == nil
+end
+
+local function on_list(options)
+	local items = options.items
+	if #items > 1 then
+		items = filter(items, filterReactDTS)
+	end
+
+	vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
+	vim.api.nvim_command("cfirst") -- or maybe you want 'copen' instead of 'cfirst'
+end
+
+-- init & attache & config LSP things
 for server_name, server in pairs(servers) do
 	server.on_init = configs.on_init
-	server.on_attach = configs.on_attach
+	server.on_attach = function(client, bufnr)
+		-- Apply the nvchad on_attach
+		configs.on_attach(client, bufnr)
+
+		-- Overwrite the "gd" keymap to remove the index.d.ts files from GD
+		local bufopts = { noremap = true, silent = true, buffer = bufnr }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition({ on_list = on_list })
+		end, bufopts)
+	end
 	server.capabilities = configs.capabilities
 	require("lspconfig")[server_name].setup(server)
 end
