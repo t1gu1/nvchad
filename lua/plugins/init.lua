a653cadfa8b58908ba3d7b691afc4d50766eceff
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-09-18 12:26:59 -0400

    Remove a readme plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,16 +4,6 @@ local gheight = vim.api.nvim_list_uis()[1].height
 local width = 60
 local height = 40
 
--- Use for Markdown/Headline
-vim.cmd([[highlight Bullet guifg=#333333]])
-vim.cmd([[highlight Headline1 guibg=#111111 guifg=#d93929 gui=bold]])
-vim.cmd([[highlight Headline2 guibg=#111111 guifg=#00ffa5 gui=bold]])
-vim.cmd([[highlight Headline3 guibg=#111111 guifg=#00ebd7]])
-vim.cmd([[highlight Headline4 guibg=#111111 guifg=#00d5f2 gui=italic]])
-vim.cmd([[highlight CodeBlock guibg=#111111]])
-vim.cmd([[highlight Dash guibg=#232323 guifg=#222222 gui=bold]])
-vim.cmd([[highlight Quote guifg=#ffffc9]])
-
 return {
 	-- Visual
 	{
@@ -194,30 +184,6 @@ return {
 		"thenbe/markdown-todo.nvim",
 		ft = { "md", "mdx", "markdown" },
 		config = true,
-	},
-	{
-		event = "VeryLazy",
-		"lukas-reineke/headlines.nvim",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("headlines").setup({
-				markdown = {
-					bullets = { "", "✸", "◉", "✿" },
-					bullet_highlights = {
-						"Bullet",
-					},
-					quote_highlight = "Quote",
-					headline_highlights = {
-						"Headline1",
-						"Headline2",
-						"Headline3",
-						"Headline4",
-						"CodeBlock",
-						"Dash",
-					},
-				},
-			})
-		end,
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
