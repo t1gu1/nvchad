c0818a7d4cb07dab0c8c266a1649266233b00fa5
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-03-06 18:55:08 -0500

    Add back git-blame

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,6 +17,16 @@ return {
 		end,
 	},
 	{
+		event = "VeryLazy",
+		"f-person/git-blame.nvim",
+		opts = {
+			enabled = true,
+			-- message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			-- date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			-- virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		},
+	},
+	{
 		"monkoose/DoNe",
 		event = "VeryLazy",
 		lazy = true,
