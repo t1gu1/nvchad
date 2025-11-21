f634a8937965ef924f44ba7f8f855812f3dad989
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2025-11-21 04:11:33 -0500

    Adds git-blame plugin
    Adds the git-blame plugin for displaying git blame information inline.
    
    Configures the plugin with default options, including enabling it and providing
    options for customizing the message template and date format.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -184,6 +184,16 @@ return {
       require("mini.surround").setup { n_lines = 260 }
     end,
   },
+  {
+    event = "VeryLazy",
+    "f-person/git-blame.nvim",
+    opts = {
+      enabled = true,
+      -- message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+      -- date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+      -- virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
 
   -- Package version
   {
