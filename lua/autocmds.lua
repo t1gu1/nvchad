8ea411204d056f200ddc9a06a428010821c2c92e
t1gu1 <guillaumehuardhughes@gmail.com>
2026-01-11 11:03:31 -0500

    Adds autocommand for Love2D setup
    Adds an autocommand that checks for a "love.xxx" require statement in the main.lua file and, if found, loads the love2d module.
    
    This allows automatic setup of the love2d environment when opening a project.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,25 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("VimEnter", {
+  callback = function()
+    local main_lua = vim.fn.getcwd() .. "/main.lua"
+    if vim.fn.filereadable(main_lua) == 1 then
+      local lines = vim.fn.readfile(main_lua)
+      for _, line in ipairs(lines) do
+        if line:find("love%.") then
+          local ok, love2d = pcall(require, "love2d")
+          if ok then
+            -- Didn't works without the defer..
+            vim.defer_fn(function()
+              love2d.setup()
+            end, 1000)
+          end
+          return
+        end
+      end
+    end
+  end,
+})
+
