baf2bf7aa90403218ba8e902fb4237bd00bf715a
t1gu1 <guillaumehuardhughes@gmail.com>
2026-01-11 10:43:43 -0500

    Updates plugin configurations and adds LÖVE support
    Refreshes the `lazy-lock.json` file to reflect the latest
    plugin versions and dependencies.
    
    Adds support for LÖVE development with debugging features,
    key mappings for running and stopping LÖVE applications.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,6 +27,8 @@ return {
     end,
   },
 
+
+
   -- test new blink
   { import = "nvchad.blink.lazyspec" },
 
@@ -99,10 +101,10 @@ return {
     config = function()
       require("noice").setup {
         presets = {
-          bottom_search = false, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
+          bottom_search = false,        -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          lsp_doc_border = false,       -- add a border to hover docs and signature help
         },
         lsp = {
           signature = {
@@ -224,9 +226,9 @@ return {
             invalid = "|  ", -- Icon for invalid dependencies
           },
         },
-        notifications = true, -- Whether to display notifications when running commands
-        autostart = true, -- Whether to autostart when `package.json` is opened
-        hide_up_to_date = false, -- It hides up to date versions when displaying virtual text
+        notifications = true,           -- Whether to display notifications when running commands
+        autostart = true,               -- Whether to autostart when `package.json` is opened
+        hide_up_to_date = false,        -- It hides up to date versions when displaying virtual text
         hide_unstable_versions = false, -- It hides unstable versions from version list e.g next-11.1.3-canary3
         -- Can be `npm`, `yarn`, or `pnpm`. Used for `delete`, `install` etc...
         -- The plugin will try to auto-detect the package manager based on
@@ -252,5 +254,21 @@ return {
         },
       }
     end,
+  },
+
+  {
+    "S1M0N38/love2d.nvim",
+    event = "VeryLazy",
+    version = "2.*",
+    opts = {
+      debug_window_opts = {
+        split = "below",
+      },
+    },
+    keys = {
+      { "<leader>l",  ft = "lua",          desc = "LÖVE" },
+      { "<leader>lr", "<cmd>LoveRun<cr>",  ft = "lua",   desc = "Run LÖVE" },
+      { "<leader>lq", "<cmd>LoveStop<cr>", ft = "lua",   desc = "Stop LÖVE" },
+    },
   },
 }
